56e1a0e5abe837ddcbaac3c6111021b948480d65
Ammad <kh.ammad123@gmail.com>
2020-04-26 12:10:01 +0500

    Added season model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_070536) do
+ActiveRecord::Schema.define(version: 2020_04_26_070833) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
+    t.text "plot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "seasons", force: :cascade do |t|
+    t.string "title"
+    t.integer "number"
     t.text "plot"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
