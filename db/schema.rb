82e265c11509125f6a512c72431bd36147cfd77e
Ammad <kh.ammad123@gmail.com>
2020-04-26 12:18:45 +0500

    Added model user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_071334) do
+ActiveRecord::Schema.define(version: 2020_04_26_071759) do
 
   create_table "episodes", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 2020_04_26_071334) do
     t.string "title"
     t.integer "number"
     t.text "plot"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
