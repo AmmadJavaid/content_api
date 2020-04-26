91e243e9f967a04ab3c5cb0c999b27025ffe11e6
Ammad <kh.ammad123@gmail.com>
2020-04-26 13:03:01 +0500

    Purchase: Added uniqness validation

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,3 +1,23 @@
 class Purchase < ApplicationRecord
   belongs_to :purchase_of, polymorphic: true
+
+  validate :check_purchase_of_uniqness
+
+  before_create :set_expiry
+
+  scope :expired, -> { where("? > expire_at", Time.now) }
+
+  def is_expiry?
+    Time.now > self.expire_at
+  end
+
+  def set_expiry
+    self.expire_at = Time.now + 2.days
+  end
+
+  def check_purchase_of_uniqness
+    if Purchase.where(purchase_of: self.purchase_of).where("expire_at < ?", Time.now).exists?
+      self.errors.add(:base, 'Item is already purchases')
+    end
+  end
 end
