d99a8ba47d7652a7b914a382b1a966ddc7d793d0
Ammad <kh.ammad123@gmail.com>
2020-04-27 11:31:52 +0500

    Added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+  Movie.create(title: Faker::Movie.quote)
+end
+
+20.times do |x|
+  s = Season.create(title: Faker::Movie.quote, number: x)
+  rand(10).times do |ep|
+    s.episodes.create(title: Faker::Movie.quote, number_in_season: ep )
+  end
+end
+
